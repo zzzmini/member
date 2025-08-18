088b86cac88b2bdfeeed2c8956c2ca2f0eb1c325
zzzmini <sm01079200887@gmail.com>
2025-08-18 17:46:52 +0900

    로그인 세션처리 완료

--- src/main/java/com/my/member/controller/UserController.java
+++ src/main/java/com/my/member/controller/UserController.java
@@ -2,6 +2,7 @@ package com.my.member.controller;
 
 import com.my.member.dto.UserDto;
 import com.my.member.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.ObjectUtils;
@@ -59,24 +60,36 @@ public class UserController {
     }
 
     @PostMapping("updateUser")
-    public String updateUser(@ModelAttribute("user")UserDto user) {
+    public String updateUser(@ModelAttribute("user") UserDto user) {
         userService.saveUser(user);
         return "redirect:/user/list";
     }
 
     @PostMapping("login")
-    public String login(UserDto dto) {
+    public String login(UserDto dto, HttpSession session) {
         // 1. dto.email을 갖고 User 검색...
         UserDto loginResult = userService.findOneUser(dto.getEmail());
         // 2. 해당 유저가 있는지 확인한다.
         if (ObjectUtils.isEmpty(loginResult)) {
             // 로그인 실패
             return "/user/login";
-        } else {
+        } else if (dto.getPassword().equals(loginResult.getPassword())) {
             // 3. password 가 맞는지 확인한다.
-            // 틀리면 : login form 보여준다.
             // 맞으면 : 세션을 만들어 준다.
+            session.setAttribute("loginEmail", dto.getEmail());
+            // 세션 유지 시간
+            session.setMaxInactiveInterval(60 * 30);
+            return "main";
+        } else {
+            // 틀리면 : login form 보여준다.
+            return "/user/login";
         }
-        return null;
+    }
+
+    @GetMapping("logout")
+    public String logout(HttpSession session) {
+        // 세션을 삭제하는 메서드
+        session.invalidate();
+        return "main";
     }
 }
